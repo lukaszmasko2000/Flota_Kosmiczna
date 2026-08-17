d48615560a132c3cf902812bb5c313a697ae6350
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 15:40:11 +0200

    Added virtual destructor, virtual function obliczZasieg and wyswietl

--- Kosmoplatan.h
+++ Kosmoplatan.h
@@ -11,7 +11,12 @@ public:
     Kosmoplatan(const std::string& _nazwa, int _masa, int _mocNapedu)
         : nazwa(_nazwa), masa(_masa), mocNapedu(_mocNapedu){}
 
-    
+    virtual ~Kosmoplatan(){
+        std::cout << "[Destruktor] Usuwanie obiektu: " << nazwa << std::endl;
+    }
+
+    virtual double obliczZasieg() const = 0;
+    virtual void wyswietl() const = 0;
 
 private:
     std::string nazwa;
