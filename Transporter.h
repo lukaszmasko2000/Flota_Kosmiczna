1c189161ed4bd982d0da98b6affbc664cc6be3c6
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 16:49:21 +0200

    Added Transporter class (it's inheritancing Kosmoplatan)

--- Transporter.h
+++ Transporter.h
@@ -2,9 +2,16 @@
 #define TRANSPORTER_H
 
 #include "Kosmoplatan.h"
+#include <iostream>
 
-
-
-
-
+class Transporter 
+    : public Kosmoplatan
+{
+public:
+    Transporter(const std::string& nazwa, int masa, int mocNapedu, int _ladownosc)
+        : Kosmoplatan(nazwa,masa,mocNapedu) , ladownosc(_ladownosc){}
+    
+private:
+    int ladownosc;
+};
 #endif //TRANSPORTER_H
