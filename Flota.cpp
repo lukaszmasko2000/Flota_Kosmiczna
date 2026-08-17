815c523229c78592725dffe316f982446bd791b2
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 21:41:03 +0200

    Some cosmetic changes

--- Flota.cpp
+++ Flota.cpp
@@ -1,1 +1,26 @@
 #include "Flota.h"
+
+
+//TODO TOMMOROW
+
+void Flota::wyswietlFlote() const{
+
+}
+void Flota::sortujPoZasiegu() const{
+
+}
+int Flota::zliczPowyzejMocy(int minMoc) const{
+
+}
+void Flota::usunCiezkie(int maxMasa){
+
+}
+int Flota::obliczCalkowitaMoc() const{
+
+}
+void Flota::pobierzSzybkieStatki(std::vector<Kosmoplatan*>& cel, double minZasieg) const{
+
+}
+void Flota::eksportujNazwy() const{
+
+}
