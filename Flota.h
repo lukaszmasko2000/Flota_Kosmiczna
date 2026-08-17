1bdc38776797f6f0660028c7edeb2820eb814459
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 21:21:23 +0200

    I made first struct as a functor with power condition

--- Flota.h
+++ Flota.h
@@ -2,10 +2,19 @@
 #define FLOTA_H
 
 
+#include "Kosmoplatan.h"
+
+
 struct KryteriumMocy{
+    int progMocy;
 
+    KryteriumMocy(int p) 
+        : progMocy(p){}
     
-
+    bool operator()(const Kosmoplatan* statek) const{
+        if (statek->getMocNapedu() > progMocy) return true;
+        return false;
+    }
 };
 
 struct EkstraktorNazwy{
