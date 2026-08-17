6a070da1be72a27715829675d8411bacb379da33
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 15:36:51 +0200

    Added Kosmoplatan class with constructon and private variables

--- Kosmoplatan.h
+++ Kosmoplatan.h
@@ -1,12 +1,21 @@
 #ifndef KOSMOPLATAN_H
 #define KOSMOPLATAN_H
 
+#include <iostream>
 
 
 
+class Kosmoplatan
+{
+public:
+    Kosmoplatan(const std::string& _nazwa, int _masa, int _mocNapedu)
+        : nazwa(_nazwa), masa(_masa), mocNapedu(_mocNapedu){}
 
+    
 
-
-
-
+private:
+    std::string nazwa;
+    int masa;
+    int mocNapedu;
+};
 #endif //KOSMOPLATAN_H
