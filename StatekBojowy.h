c72ceb23584994e051c1b02ec99d68bfd0e03197
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 16:24:00 +0200

    Added StatekBojowy class (it's inheritancing Kosmoplatan)

--- StatekBojowy.h
+++ StatekBojowy.h
@@ -2,10 +2,20 @@
 #define STATEKBOJOWY_H
 
 #include "Kosmoplatan.h"
+#include <iostream>
 
 
+class StatekBojowy 
+: public Kosmoplatan
+{
+public:
+    StatekBojowy(const std::string& nazwa, int masa, int mocNapedu, int _silaOgnia)
+        : Kosmoplatan(nazwa,masa,mocNapedu), silaOgnia(_silaOgnia){}
+    
+    double obliczZasieg() const override;
+    void wyswietl() const override;
 
-
-
-
+private:
+    int silaOgnia;
+};
 #endif //STATEKBOJOWY_H
