8b496bb80d81a0383e01df19afb6d9fc1e46926f
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-17 16:58:12 +0200

    Added math stuff inside obliczZasieg function

--- Transporter.cpp
+++ Transporter.cpp
@@ -1,4 +1,12 @@
 #include "Transporter.h"
 
 
-
+double Transporter::obliczZasieg() const
+{
+    auto oblicz{static_cast<double>((static_cast<double>(getMocNapedu()) * 150) / (getMasa() + ladownosc * 0.5))};
+    return oblicz;
+}
+void Transporter::wyswietl() const
+{
+    
+}
