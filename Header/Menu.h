c8d310e639e52795090fee884acebab6eb005cde
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-19 21:10:55 +0200

    Added preprocessor commands with empty Menu class

---
+++ Header/Menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+#include "Flota.h"
+#include "Kosmoplatan.h"
+#include "StatekBojowy.h"
+#include "Transporter.h"
+
+class Menu
+{
+
+
+
+
+
+};
+#endif // MENU_H
