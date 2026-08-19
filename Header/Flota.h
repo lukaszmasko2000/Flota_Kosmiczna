8ab2293f81a22ed4dd5df93d5c1a05c476264748
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-19 12:35:10 +0200

    Removed 2 useless structs

--- Header/Flota.h
+++ Header/Flota.h
@@ -16,29 +16,6 @@
 
 using namespace std::string_literals;
 
-
-struct KryteriumMocy{
-    int progMocy;
-
-    KryteriumMocy(int p) 
-        : progMocy(p){}
-    
-    bool operator()(const Kosmoplatan* statek) const{
-        if (statek->getMocNapedu() > progMocy) return true;
-        return false;
-    }
-};
-
-struct EkstraktorNazwy{
-
-    std::string operator()(const Kosmoplatan* statek) const{
-        if (statek) return statek->getNazwa();
-        return ""s;
-    }
-
-};
-
-
 class Flota
 {
 public:
