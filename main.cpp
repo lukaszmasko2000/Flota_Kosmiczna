aee11fe9fa8e632eb37dd63b0372704c57271279
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-19 11:22:23 +0200

    I changed tests for smart pointers

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main() {
      std::cout << "=== TEST ROZPOCZETY (Alokacja obiektow) ===" << std::endl;
     
     // Blok ograniczający czas życia floty, w celu weryfikacji automatycznego czyszczenia pamięci
+    std::cout << "=== TEST ROZPOCZETY (Bezpieczna alokacja unique_ptr) ===" << std::endl;
+    
     {
         Flota flota;
 
-        // Dynamiczna alokacja elementów przez surowe wskaźniki
-        flota.dodajStatek(new StatekBojowy("X-Wing", 15, 500, 5));      
-        flota.dodajStatek(new Transporter("Cargomaster", 120, 800, 80)); 
-        flota.dodajStatek(new StatekBojowy("Niszczyciel", 450, 2500, 50)); 
-        flota.dodajStatek(new Transporter("Shuttle-T4", 45, 400, 20));    
+        flota.dodajStatek(std::make_unique<StatekBojowy>("X-Wing", 15, 500, 5));      
+        flota.dodajStatek(std::make_unique<Transporter>("Cargomaster", 120, 800, 80)); 
+        flota.dodajStatek(std::make_unique<StatekBojowy>("Niszczyciel", 450, 2500, 50)); 
+        flota.dodajStatek(std::make_unique<Transporter>("Shuttle-T4", 45, 400, 20));    
 
         std::cout << "\n--- Stan Floty Poczatkowy ---" << std::endl;
         flota.wyswietlFlote();
@@ -31,29 +32,23 @@ int main() {
         std::cout << "\n--- Test sumowania mocy (std::plus + std::bind) ---" << std::endl;
         std::cout << "Calkowita moc floty: " << flota.obliczCalkowitaMoc() << " MW (Oczekiwano: 4200 MW)" << std::endl;
 
-        std::cout << "\n--- Test adaptera back_inserter (Statki z zasiegiem >= 1000) ---" << std::endl;
-        std::vector<Kosmoplatan*> szybkaFlota;
-        flota.pobierzSzybkieStatki(szybkaFlota, 1000.0);
-        
-        std::for_each(szybkaFlota.begin(), szybkaFlota.end(), [](const Kosmoplatan* s) {
-            if (s) std::cout << " -> Szybki statek: " << s->getNazwa() << " (Zasieg: " << s->obliczZasieg() << ")\n";
-        });
+        std::cout << "\n--- Test partition i wyswietlania (Statki z zasiegiem >= 1000) ---" << std::endl;
+        flota.pobierzSzybkieStatki(1000.0);
 
         std::cout << "\n--- Eksport nazw wszystkich statkow (ostream_iterator) ---" << std::endl;
         flota.eksportujNazwy();
 
         std::cout << "\n--- Usuniecie statkow ciezszych niz 100t ---" << std::endl;
-        // Metoda powinna usunąć z pamięci i wywołać destruktory dla: Cargomaster oraz Niszczyciel
         flota.usunCiezkie(100); 
         
         std::cout << "\n--- Stan Floty po usunieciu ---" << std::endl;
         flota.wyswietlFlote();
         
         std::cout << "\n=== Koniec bloku lokalnego (Flota zaraz opusci zasieg) ===" << std::endl;
-    } // <- Tutaj destruktor floty usuwa pozostałe statki: X-Wing oraz Shuttle-T4.
+    } 
     
     std::cout << "\n=== TEST ZAKONCZONY ===" << std::endl;
-    std::cout << "Weryfikacja: Odnotowanie na ekranie dokladnie 4 komunikatów [Destruktor] oznacza brak wyciekow." << std::endl;
+    std::cout << "Weryfikacja: Wszystkie 4 obiekty zwolnione w odpowiednich momentach cyklu zycia programu." << std::endl;
 
     return 0;
 }
