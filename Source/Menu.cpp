b57bb384cf55f3b78f7f676d1bd0db5ca5a095eb
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-20 18:18:08 +0200

    Some changes in wyswietlMenu

--- Source/Menu.cpp
+++ Source/Menu.cpp
@@ -8,19 +8,20 @@
 
 void Menu::wyswietlMenu() const
 {
-    std::cout << std::setfill('=') << std::setw(50) << "" << std::endl;
+    std::cout << std::setfill('=') << std::setw(50) << "" << '\n';
     std::cout << "=== MENU GLOWNE ===" << std::endl;
-    std::cout << std::setfill('=') << std::setw(50) << "" << std::endl;
-    std::cout << "1. Dodaj statek bojowy" << std::endl;
-    std::cout << "2. Dodaj transporter" << std::endl;
-    std::cout << "3. Wyswietl flote" << std::endl;
-    std::cout << "4. Sortuj po zasiegu" << std::endl
-              << "5. Zlicz statki powyzej mocy" << std::endl
-              << "6. Usun ciezkie statki" << std::endl
-              << "7. Oblicz calkowita moc floty" << std::endl
-              << "8. Pobierz szybkie statki" << std::endl
-              << "9. Eksport nazw wszystkich statkow" << std::endl
-              << "0. Wyjscie z programu" << std::endl;
+    std::cout << std::setfill('=') << std::setw(50) << "" << '\n';
+    std::cout << R"(1. Dodaj statek bojowy
+                    2. Dodaj transporter
+                    3. Sortuj po zasiegu
+                    4. Sortuj po zasiegu
+                    5. Zlicz statki powyzej mocy
+                    6. Usun ciezkie statki
+                    7. Oblicz calkowita moc floty
+                    8. Pobierz szybkie statki
+                    9. Eksport nazw wszystkich statkow
+                    0. Wyjscie z programu
+                    )";
 }
 
 void Menu::uruchomMenu()
