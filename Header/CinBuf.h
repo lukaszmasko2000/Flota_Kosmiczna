2a74291e1994ea2287402b75271f29093bb8d2fe
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-20 12:23:13 +0200

    Added pressEnter() function

--- Header/CinBuf.h
+++ Header/CinBuf.h
@@ -1,7 +1,18 @@
 #ifndef CIN_BUF_H
 #define CIN_BUF_H
 
+#include <iostream>
 
+class CinBuf
+{
+public:
+    CinBuf() = default;
+    ~CinBuf() = default;
+
+    void pressEnter() const;
+
+
+};
 
 
 
