1a2b8f645e36d65d72def2d1a89c64027f84d460
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-21 13:41:45 +0200

    Added undorered_map to change switch case condition soon

--- Header/Menu.h
+++ Header/Menu.h
@@ -47,5 +47,18 @@ private:
     Flota flota;
     CinBuf buff;
 
+    std::unordered_map<Choose_Option, std::function<void()>> actions {
+        {Choose_Option::Add_Warship, [this] { buff.clearScreen(); addStatekBojowy(); buff.pressEnter(); }},
+        {Choose_Option::Add_Transporter, [this] { buff.clearScreen(); addTransporter(); buff.pressEnter(); }},
+        {Choose_Option::Show_The_Navy, [this] { buff.clearScreen(); flota.wyswietlFlote(); buff.pressEnter(); }},
+        {Choose_Option::Sort_By_Range, [this] { buff.clearScreen(); flota.sortujPoZasiegu(); buff.pressEnter(); }},
+        {Choose_Option::Count_Starships_Above_Power, [this] { buff.clearScreen(); zliczStatkiPowyzejMocy(); buff.pressEnter(); }},
+        {Choose_Option::Remove_Heavy_Starships, [this] { buff.clearScreen(); usunCiezkieStatki(); buff.pressEnter(); }},
+        {Choose_Option::Calculate_The_Total_Power, [this] { buff.clearScreen(); std::print("Całkowita moc floty: {} MW\n", flota.obliczCalkowitaMoc()); buff.pressEnter(); }},
+        {Choose_Option::Download_Fast_Starships, [this] { buff.clearScreen(); pobierzSzybkieStatki(); buff.pressEnter(); }},
+        {Choose_Option::Export_Every_Starship_Name, [this] { buff.clearScreen(); flota.eksportujNazwy();; buff.pressEnter(); }},
+        {Choose_Option::Exit, [this] { buff.clearScreen(); std::print("Wyjście z programu.\n"); buff.pressEnter(); }}
+    };
+
 };
 #endif // MENU_H
